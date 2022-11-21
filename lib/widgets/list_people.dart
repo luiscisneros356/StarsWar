79548bec109b0429e16f37e970a9dcb0a76fa176
Luis Cisneros <87794700+luiscisneros356@users.noreply.github.com>
2022-11-21 04:08:59 -0300

    animaciones

--- lib/widgets/list_people.dart
+++ lib/widgets/list_people.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 
 import '../provider/provider.dart';
 import '../routes/routes.dart';
+import 'bottom.dart';
 import 'card_people.dart';
 
 class ListPeople extends StatefulWidget {
@@ -16,18 +16,22 @@ class ListPeople extends StatefulWidget {
   State<ListPeople> createState() => _ListPeopleState();
 }
 
-class _ListPeopleState extends State<ListPeople> {
+class _ListPeopleState extends State<ListPeople> with SingleTickerProviderStateMixin {
   late PageController _controller;
+  late AnimationController _animationController;
+  late Animation<double> _opacity;
 
   late Duration _duration;
   late Curve _curves;
-  double d = 0.0;
+  double _currentPage = 0.0;
   @override
   void initState() {
     _controller = PageController();
+    _animationController = AnimationController(vsync: this, duration: Duration(seconds: 1));
 
+    _opacity = Tween(begin: 0.0, end: 1.0).animate(_animationController);
     _controller.addListener(() {
-      d = _controller.page ?? 0.0;
+      _currentPage = _controller.page ?? 0.0;
       setState(() {});
     });
     _duration = const Duration(seconds: 1);
@@ -46,57 +50,52 @@ class _ListPeopleState extends State<ListPeople> {
         height: mq.height / 2,
         width: mq.width,
         child: Stack(children: [
-          PageView.builder(
-              controller: _controller,
-              itemCount: prov.listPeople.length,
-              itemBuilder: ((context, i) {
-                final people = prov.listPeople[i];
-                final scale = 1 - (d - i);
-                scale.clamp(0.0, 1.0);
-                return GestureDetector(
-                    onTap: (() {
-                      Navigator.pushNamed(context, RoutesApp.details);
-                      prov.setPeople(people);
-                      prov.setIsBackFromDetail(true);
-                    }),
-                    child: Hero(
-                        tag: people.name,
-                        child: AnimatedScale(
-                            duration: const Duration(milliseconds: 100),
-                            scale: scale,
-                            child: PeopleCard(people: people))));
-              })),
+          AnimatedBuilder(
+              animation: _animationController,
+              builder: (BuildContext context, Widget? child) {
+                return PageView.builder(
+                    controller: _controller,
+                    itemCount: prov.listPeople.length,
+                    itemBuilder: ((context, i) {
+                      final people = prov.listPeople[i];
+                      final scale = 1 - (_currentPage - i);
+
+                      scale.clamp(0.0, 1.0);
+
+                      return GestureDetector(
+                        onTap: (() {
+                          Navigator.pushNamed(context, RoutesApp.details);
+                          prov.setPeople(people);
+                          prov.setIsBackFromDetail(true);
+                        }),
+                        child: Hero(
+                            tag: people.name,
+                            child: AnimatedOpacity(
+                              opacity: _opacity.value == 0 ? 1 : _opacity.value,
+                              duration: const Duration(milliseconds: 100),
+                              child: AnimatedScale(
+                                  duration: const Duration(milliseconds: 100),
+                                  scale: scale,
+                                  child: PeopleCard(people: people)),
+                            )),
+                      );
+                    }));
+              }),
           Botton(
               icon: Icons.arrow_back_ios,
-              function: () => _controller.previousPage(duration: _duration, curve: _curves),
+              function: () {
+                _animationController.forward(from: 0.0);
+                _controller.previousPage(duration: _duration, curve: _curves);
+              },
               alignment: Alignment.centerLeft),
           Botton(
               icon: Icons.arrow_forward_ios,
-              function: () => _controller.nextPage(duration: _duration, curve: _curves),
+              function: () {
+                _animationController.forward(from: 0.0);
+                _controller.nextPage(duration: _duration, curve: _curves);
+              },
               alignment: Alignment.centerRight),
         ]),
-      ),
-    );
-  }
-}
-
-class Botton extends StatelessWidget {
-  const Botton({Key? key, required this.icon, required this.function, required this.alignment})
-      : super(key: key);
-  final IconData icon;
-  final VoidCallback function;
-  final Alignment alignment;
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: alignment,
-      child: IconButton(
-        icon: Icon(
-          icon,
-          size: 50,
-        ),
-        onPressed: function,
       ),
     );
   }
